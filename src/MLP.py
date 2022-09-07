e1572150d0f74e3ad5f0c8feed236965c3b222a4
Eduardo Corpeño <kuashio@gmail.com>
2022-09-07 13:20:20 -0600

    updated for Codespaces

--- src/MLP.py
+++ src/MLP.py
@@ -4,7 +4,7 @@ class Perceptron:
     """A single neuron with the sigmoid activation function.
        Attributes:
           inputs: The number of inputs in the perceptron, not counting the bias.
-          bias:   The bias term. By defaul it's 1.0."""
+          bias:   The bias term. By default it's 1.0."""
 
     def __init__(self, inputs, bias = 1.0):
         """Return a new Perceptron object with the specified number of inputs (+1 for the bias).""" 
@@ -13,8 +13,8 @@ class Perceptron:
 
     def run(self, x):
         """Run the perceptron. x is a python list with the input values."""
-        sum = np.dot(np.append(x,self.bias),self.weights)
-        return self.sigmoid(sum)
+        x_sum = np.dot(np.append(x,self.bias),self.weights)
+        return self.sigmoid(x_sum)
 
     def set_weights(self, w_init):
         """Set the weights. w_init is a python list with the weights."""
@@ -55,7 +55,8 @@ class MultiLayerPerceptron:
 
     def set_weights(self, w_init):
         # Write all the weights into the neural network.
-        # w_init is a list of floats. Organize it as you'd like.        
+        # w_init is a list of floats. Organize it as you'd like. 
+        pass       
 
     def printWeights(self):
         print()
@@ -65,7 +66,7 @@ class MultiLayerPerceptron:
         print()
 
     def run(self, x):
-        # Run an input forward through the neural network
+        # Run an input forward through the neural network.
         # x is a python list with the input values.
         return self.values[-1]
         
