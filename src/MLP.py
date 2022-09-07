0b5689b78af324de10f52304ad9d940c5b001cc4
Eduardo Corpeño <kuashio@gmail.com>
2022-09-07 14:01:29 -0600

    updated for Codespaces

--- src/MLP.py
+++ src/MLP.py
@@ -5,7 +5,7 @@ class Perceptron:
     """A single neuron with the sigmoid activation function.
        Attributes:
           inputs: The number of inputs in the perceptron, not counting the bias.
-          bias:   The bias term. By defaul it's 1.0."""
+          bias:   The bias term. By default it's 1.0."""
 
     def __init__(self, inputs, bias=1.0):
         """Return a new Perceptron object with the specified number of inputs (+1 for the bias)."""
@@ -14,8 +14,8 @@ class Perceptron:
 
     def run(self, x):
         """Run the perceptron. x is a python list with the input values."""
-        sum = np.dot(np.append(x, self.bias), self.weights)
-        return self.sigmoid(sum)
+        x_sum = np.dot(np.append(x, self.bias), self.weights)
+        return self.sigmoid(x_sum)
 
     def set_weights(self, w_init):
         """Set the weights. w_init is a python list with the weights."""
